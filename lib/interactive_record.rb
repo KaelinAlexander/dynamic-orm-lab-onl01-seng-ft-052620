3e28286d3d9edba6ab002eafa66f03711fbdcd60
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-29 18:23:02 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -62,7 +62,6 @@ class InteractiveRecord
 
   def self.find_by(student_hash)
     sql = "SELECT * FROM #{self.table_name} WHERE ? = '?'"
-    binding.pry
     DB[:conn].execute(sql, attr.keys[0].to_s, attr.values[0])
   end
 

2752854710bb3ae284a9a5f07a6a80eb3666d294
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-29 17:36:36 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,8 +60,8 @@ class InteractiveRecord
   end
 
   def self.find_by(row, value)
-    sql = "SELECT * FROM #{self.table_name} WHERE ? = '#{name}'"
-    DB[:conn].execute(sql, select)
+    sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
+    DB[:conn].execute(sql, row, value)
   end
 
 end
